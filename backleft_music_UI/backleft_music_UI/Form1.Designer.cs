307288f65aca4d15b883c9b7c80ff29ce7736b8d
Joe Caruso <joseph.caruso@mymail.champlain.edu>
2014-04-14 19:23:03 -0400

    tables

--- backleft_music_UI/backleft_music_UI/Form1.Designer.cs
+++ backleft_music_UI/backleft_music_UI/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(libForm));
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -45,23 +44,10 @@
             this.playlist_songBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.playlist_songTableAdapter = new backleft_music_UI.mydbDataSetTableAdapters.playlist_songTableAdapter();
             this.tableAdapterManager = new backleft_music_UI.mydbDataSetTableAdapters.TableAdapterManager();
-            this.playlist_songBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.playlist_songBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.songinfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.songinfoTableAdapter = new backleft_music_UI.mydbDataSetTableAdapters.songinfoTableAdapter();
             this.songinfoDataGridView = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -76,8 +62,6 @@
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mydbDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.playlist_songBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.playlist_songBindingNavigator)).BeginInit();
-            this.playlist_songBindingNavigator.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.songinfoBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.songinfoDataGridView)).BeginInit();
             this.SuspendLayout();
@@ -214,130 +198,6 @@
             this.tableAdapterManager.userplaylistTableAdapter = null;
             this.tableAdapterManager.userpurchasesTableAdapter = null;
             // 
-            // playlist_songBindingNavigator
-            // 
-            this.playlist_songBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
-            this.playlist_songBindingNavigator.BindingSource = this.playlist_songBindingSource;
-            this.playlist_songBindingNavigator.CountItem = this.bindingNavigatorCountItem;
-            this.playlist_songBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
-            this.playlist_songBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bindingNavigatorMoveFirstItem,
-            this.bindingNavigatorMovePreviousItem,
-            this.bindingNavigatorSeparator,
-            this.bindingNavigatorPositionItem,
-            this.bindingNavigatorCountItem,
-            this.bindingNavigatorSeparator1,
-            this.bindingNavigatorMoveNextItem,
-            this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem,
-            this.playlist_songBindingNavigatorSaveItem});
-            this.playlist_songBindingNavigator.Location = new System.Drawing.Point(0, 0);
-            this.playlist_songBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
-            this.playlist_songBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
-            this.playlist_songBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
-            this.playlist_songBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
-            this.playlist_songBindingNavigator.Name = "playlist_songBindingNavigator";
-            this.playlist_songBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.playlist_songBindingNavigator.Size = new System.Drawing.Size(627, 25);
-            this.playlist_songBindingNavigator.TabIndex = 9;
-            this.playlist_songBindingNavigator.Text = "bindingNavigator1";
-            // 
-            // bindingNavigatorMoveFirstItem
-            // 
-            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
-            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
-            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveFirstItem.Text = "Move first";
-            // 
-            // bindingNavigatorMovePreviousItem
-            // 
-            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
-            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
-            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
-            // 
-            // bindingNavigatorSeparator
-            // 
-            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorPositionItem
-            // 
-            this.bindingNavigatorPositionItem.AccessibleName = "Position";
-            this.bindingNavigatorPositionItem.AutoSize = false;
-            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
-            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
-            this.bindingNavigatorPositionItem.Text = "0";
-            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
-            // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
-            // bindingNavigatorSeparator1
-            // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorMoveNextItem
-            // 
-            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
-            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
-            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveNextItem.Text = "Move next";
-            // 
-            // bindingNavigatorMoveLastItem
-            // 
-            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
-            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
-            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveLastItem.Text = "Move last";
-            // 
-            // bindingNavigatorSeparator2
-            // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
-            // 
-            // playlist_songBindingNavigatorSaveItem
-            // 
-            this.playlist_songBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.playlist_songBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("playlist_songBindingNavigatorSaveItem.Image")));
-            this.playlist_songBindingNavigatorSaveItem.Name = "playlist_songBindingNavigatorSaveItem";
-            this.playlist_songBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
-            this.playlist_songBindingNavigatorSaveItem.Text = "Save Data";
-            this.playlist_songBindingNavigatorSaveItem.Click += new System.EventHandler(this.playlist_songBindingNavigatorSaveItem_Click);
-            // 
             // songinfoBindingSource
             // 
             this.songinfoBindingSource.DataMember = "songinfo";
@@ -352,7 +212,7 @@
             this.songinfoDataGridView.AutoGenerateColumns = false;
             this.songinfoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.songinfoDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
+            this.col1,
             this.dataGridViewTextBoxColumn2,
             this.dataGridViewTextBoxColumn3,
             this.dataGridViewTextBoxColumn4,
@@ -364,17 +224,16 @@
             this.dataGridViewTextBoxColumn10,
             this.dataGridViewTextBoxColumn11});
             this.songinfoDataGridView.DataSource = this.songinfoBindingSource;
-            this.songinfoDataGridView.Location = new System.Drawing.Point(3, 3);
+            this.songinfoDataGridView.Location = new System.Drawing.Point(0, 0);
             this.songinfoDataGridView.Name = "songinfoDataGridView";
             this.songinfoDataGridView.Size = new System.Drawing.Size(586, 339);
             this.songinfoDataGridView.TabIndex = 0;
             this.songinfoDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.songinfoDataGridView_CellContentClick);
             // 
-            // dataGridViewTextBoxColumn1
+            // col1
             // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "idSongInfo";
-            this.dataGridViewTextBoxColumn1.HeaderText = "idSongInfo";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.col1.HeaderText = "";
+            this.col1.Name = "col1";
             // 
             // dataGridViewTextBoxColumn2
             // 
@@ -441,7 +300,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(627, 454);
-            this.Controls.Add(this.playlist_songBindingNavigator);
             this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.removeButton);
@@ -458,9 +316,6 @@
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mydbDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.playlist_songBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.playlist_songBindingNavigator)).EndInit();
-            this.playlist_songBindingNavigator.ResumeLayout(false);
-            this.playlist_songBindingNavigator.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.songinfoBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.songinfoDataGridView)).EndInit();
             this.ResumeLayout(false);
@@ -485,23 +340,10 @@
         private System.Windows.Forms.BindingSource playlist_songBindingSource;
         private mydbDataSetTableAdapters.playlist_songTableAdapter playlist_songTableAdapter;
         private mydbDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private System.Windows.Forms.BindingNavigator playlist_songBindingNavigator;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
-        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
-        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.ToolStripButton playlist_songBindingNavigatorSaveItem;
         private mydbDataSetTableAdapters.songinfoTableAdapter songinfoTableAdapter;
         private System.Windows.Forms.BindingSource songinfoBindingSource;
         private System.Windows.Forms.DataGridView songinfoDataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
