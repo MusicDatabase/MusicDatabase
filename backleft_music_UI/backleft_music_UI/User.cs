7a11a76b72757caa00199d24a857f51d5c0c73e1
alexDatabase <gobstoppers118@yahoo.com>
2014-04-23 15:58:38 -0400

    Adding user class

---
+++ backleft_music_UI/backleft_music_UI/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backleft_music_UI
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+        public int addressID { get; set; }
+        public int purchasesID { get; set; }
+        public int creditCardID { get; set; }
+
+    }
+}
